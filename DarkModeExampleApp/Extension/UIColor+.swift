56f3de223c4d5e8c6836348f4ff01018bf4727b6
ie-naraki <36832932+ie-naraki@users.noreply.github.com>
2019-08-07 14:49:53 +0900

    UILabelの文字色、背景色に対してColorAssetから色を設定するパターンとコードから色を設定する2パターンを実装した

--- DarkModeExampleApp/Extension/UIColor+.swift
+++ DarkModeExampleApp/Extension/UIColor+.swift
@@ -10,6 +10,18 @@ import UIKit
 
 extension UIColor {
 
+    convenience init(hex: String, alpha: CGFloat) {
+                let v = Int("000000" + hex, radix: 16) ?? 0
+                let r = CGFloat(v / Int(powf(256, 2)) % 256) / 255
+                let g = CGFloat(v / Int(powf(256, 1)) % 256) / 255
+                let b = CGFloat(v / Int(powf(256, 0)) % 256) / 255
+                self.init(red: r, green: g, blue: b, alpha: min(max(alpha, 0), 1))
+            }
+
+    convenience init(hex: String) {
+        self.init(hex: hex, alpha: 1.0)
+    }
+
     /// ダイナミックカラーを取得する
     /// - Parameter light: ライトモード時の色
     /// - Parameter dark: ダークモード時の色
@@ -24,6 +36,21 @@ extension UIColor {
             }
         }
         return light
+    }
+}
+
+extension UIColor {
+
+    static var defaultLabelBackground: UIColor {
+        return dynamicColor(light: UIColor(hex: "FFFFA6"), dark: UIColor(hex: "FFBD2C"))
+    }
+
+    static var defaultLabelForeground: UIColor {
+        return dynamicColor(light: UIColor(hex: "333333"), dark: UIColor(hex: "FFFFFF"))
+    }
+
+    static var defaultLabelBorder: UIColor {
+        return dynamicColor(light: UIColor(hex: "FCC634"), dark: UIColor(hex: "FFFAB3"))
     }
 
 }
--- DarkModeExampleApp/ViewController.swift
+++ DarkModeExampleApp/ViewController.swift
@@ -10,15 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Constants
+
+    private let defaultCornerRadius: CGFloat = 5
+    private let defaultBorderWidth: CGFloat = 2
+    private let defaultBorderColor: UIColor = UIColor.dynamicColor(light: UIColor(hex: "FCC634"),
+                                                                   dark: UIColor(hex: "FFFAB3"))
+
     // MARK: - IBOutlet
     
     @IBOutlet private weak var currentUserInterfaceStyleLabel: UILabel!
+    @IBOutlet private weak var fromAssetLabel: UILabel! {
+        didSet {
+            fromAssetLabel.layer.cornerRadius = defaultCornerRadius
+            fromAssetLabel.layer.borderWidth = defaultBorderWidth
+            // cgColorはダイナミックに変化しない？
+            fromAssetLabel.layer.borderColor = defaultBorderColor.cgColor
+            fromAssetLabel.clipsToBounds = true
+        }
+    }
+    @IBOutlet private weak var fromCodeLabel: UILabel! {
+        didSet {
+            fromCodeLabel.layer.cornerRadius = defaultCornerRadius
+            fromCodeLabel.layer.borderWidth = defaultBorderWidth
+            fromCodeLabel.layer.borderColor = defaultBorderColor.cgColor
+            fromCodeLabel.clipsToBounds = true
+            // dynamic color
+            fromCodeLabel.backgroundColor = UIColor.defaultLabelBackground
+            fromCodeLabel.textColor = UIColor.defaultLabelForeground
+        }
+    }
 
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setCurrentUserInterfaceStyle(style: nil)
+    }
+
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        // モードが変わったときにdynamicColorを再代入する
+        fromAssetLabel.layer.borderColor = defaultBorderColor.cgColor
+        fromCodeLabel.layer.borderColor = defaultBorderColor.cgColor
     }
 
     // MARK: - IBAction
@@ -31,6 +64,7 @@ class ViewController: UIViewController {
 }
 
 private extension ViewController {
+
     func setCurrentUserInterfaceStyle(style: UIUserInterfaceStyle?) {
         overrideUserInterfaceStyle = style ?? .unspecified
 
